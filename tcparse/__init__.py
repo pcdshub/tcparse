e74b475fc44b2be047c8c43b372c821c739b46f6
K Lauer <klauer@slac.stanford.edu>
2019-06-12 15:57:01 -0700

    MNT: update __init__ for easier usage as a package

--- tcparse/__init__.py
+++ tcparse/__init__.py
@@ -1,3 +1,9 @@
 from ._version import get_versions
+from .parse import load_project, TWINCAT_TYPES
+
 __version__ = get_versions()['version']
 del get_versions
+
+globals().update(TWINCAT_TYPES)
+
+__all__ = ['__version__', 'load_project'] + list(TWINCAT_TYPES)
